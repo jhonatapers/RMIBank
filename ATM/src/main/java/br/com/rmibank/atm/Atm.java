475594238ab0aaf6dfb00cd641a518956749b63e
Jhonata Peres <jhonatapers@gmail.com>
2022-10-14 19:28:21 -0300

    Estrutura base do projeto criada

---
+++ ATM/src/main/java/br/com/rmibank/atm/Atm.java
@@ -0,0 +1,7 @@
+package main.java.br.com.rmibank.corebanking;
+
+public class Atm {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Atm!");
+    }
+}
---
+++ BankBranch/src/main/java/br/com/rmibank/corebanking/BankBranch.java
@@ -0,0 +1,7 @@
+package main.java.br.com.rmibank.corebanking;
+
+public class BankBranch {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, BankBranch!");
+    }
+}
