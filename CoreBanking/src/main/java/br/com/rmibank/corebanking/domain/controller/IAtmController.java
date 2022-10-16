fec91d460804c211024d50f5437d28cb035c46e1
Jhonata Peres <jhonatapers@gmail.com>
2022-10-16 11:20:58 -0300

    Corrige classe para extender Remote

--- CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IAtmController.java
+++ CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IAtmController.java
@@ -1,13 +1,14 @@
 package main.java.br.com.rmibank.corebanking.domain.controller;
 
 import java.math.BigDecimal;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 import main.java.br.com.rmibank.corebanking.domain.entity.Transacao;
 import main.java.br.com.rmibank.corebanking.domain.entity.aggregate.ContaCorrente;
 
-public interface IAtmController {
+public interface IAtmController extends Remote {
 
     public Transacao saque(int agencia, long codigoContaCorrente, BigDecimal valor) throws RemoteException;
 
