8a16a2e9d65136d7ce62bd00bd4ec7d572c45437
Jhonata Peres <jhonatapers@gmail.com>
2022-10-18 19:30:49 -0300

    Identacao

--- ATM/src/main/java/br/com/rmibank/corebanking/Atm.java
+++ ATM/src/main/java/br/com/rmibank/corebanking/Atm.java
@@ -7,10 +7,13 @@ import main.java.br.com.rmibank.corebanking.domain.controller.IAtmController;
 
 public class Atm {
     public static void main(String[] args) throws Exception {
-        IAtmController atmController = (IAtmController) Naming.lookup("rmi://10.32.143.125:1099/AtmController");
+
+        IAtmController atmController = (IAtmController) Naming
+                .lookup("rmi://localhost:1099/AtmController");
 
         MenuViewAtm menu = new MenuViewAtm(atmController);
 
         menu.start();
+
     }
 }
