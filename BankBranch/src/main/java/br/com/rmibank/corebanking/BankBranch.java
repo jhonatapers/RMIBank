1fd80c1d961befaa18fd246c1a474345ccb7134d
Jhonata Peres <jhonatapers@gmail.com>
2022-10-18 19:33:15 -0300

    remove setProperty() nao utilizada

--- BankBranch/src/main/java/br/com/rmibank/corebanking/BankBranch.java
+++ BankBranch/src/main/java/br/com/rmibank/corebanking/BankBranch.java
@@ -8,12 +8,11 @@ import main.java.br.com.rmibank.corebanking.domain.controller.IAgenciaController
 public class BankBranch {
     public static void main(String[] args) throws Exception {
 
-        System.setProperty("java.rmi.server.useCodebaseOnly", "false");
-
         IAgenciaController agenciaController = (IAgenciaController) Naming
-                .lookup("rmi://10.32.143.125:1099/AgenciaController");
+                .lookup("rmi://localhost:1099/AgenciaController");
 
         MenuViewAgencia menu = new MenuViewAgencia(agenciaController);
+
         menu.start();
 
     }
