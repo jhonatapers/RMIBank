d1ded46c26770f9b860ab8baae73440a57b084bf
Jhonata Peres <jhonatapers@gmail.com>
2022-10-19 20:19:22 -0300

    Adiciona throws

--- CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IIdempotencyController.java
+++ CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IIdempotencyController.java
@@ -1,9 +1,10 @@
 package main.java.br.com.rmibank.corebanking.domain.controller;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface IIdempotencyController extends Remote {
 
-    public int newIdempotency();
+    public int newIdempotency() throws RemoteException;
 
 }
--- CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/impl/IdempotencyController.java
+++ CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/impl/IdempotencyController.java
@@ -1,18 +1,22 @@
 package main.java.br.com.rmibank.corebanking.domain.controller.impl;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 import main.java.br.com.rmibank.corebanking.domain.controller.IIdempotencyController;
 import main.java.br.com.rmibank.corebanking.domain.service.IIdempotencyService;
 
-public class IdempotencyController implements IIdempotencyController {
+public class IdempotencyController extends UnicastRemoteObject implements IIdempotencyController {
 
     private IIdempotencyService idempotencyService;
 
-    public IdempotencyController(IIdempotencyService idempotencyService) {
+    public IdempotencyController(IIdempotencyService idempotencyService) throws RemoteException {
+        super();
         this.idempotencyService = idempotencyService;
     }
 
     @Override
-    public int newIdempotency() {
+    public int newIdempotency() throws RemoteException {
         return idempotencyService.newIdempotency();
     }
 
