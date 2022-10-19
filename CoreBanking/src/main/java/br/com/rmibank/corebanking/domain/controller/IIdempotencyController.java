1185277fd26265e000bb66bb82e0af03652ee9f4
Jhonata Peres <jhonatapers@gmail.com>
2022-10-19 20:14:50 -0300

    Adicona extends Remote

--- CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IIdempotencyController.java
+++ CoreBanking/src/main/java/br/com/rmibank/corebanking/domain/controller/IIdempotencyController.java
@@ -1,6 +1,8 @@
 package main.java.br.com.rmibank.corebanking.domain.controller;
 
-public interface IIdempotencyController {
+import java.rmi.Remote;
+
+public interface IIdempotencyController extends Remote {
 
     public int newIdempotency();
 
